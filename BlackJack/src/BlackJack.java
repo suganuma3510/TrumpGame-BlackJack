ae4cde617a1e6ac2d47a2e48a25f39b7c51728c7
Mikoto Suganuma <Suganuma3510@gmail.com>
2019-12-09 21:41:59 +0900

    コードの再利用とインターフェースの追加

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -1,7 +1,31 @@
 
+import java.util.ArrayList;
+
 public class BlackJack {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        try {
+            //プレイヤーの生成
+            ArrayList<Player> player = new ArrayList<Player>();
+            player.add(new Player("村田"));
+            player.add(new Player("山田"));
+            //進行役の生成
+            Dealer dealer = new Dealer();
+            //山札の生成
+            Deck deck = new Deck();
+            //場の生成
+            Table field = new Table();
+            //進行役に場、プレイヤー、山札を渡しゲームの準備をする
+            dealer.prepareGame(player, deck, field);
+            //ゲームの開始
+            dealer.startGame();
+            //ゲーム結果
+            dealer.result();
+        } catch (Exception e) {
+            //例外処理
+            System.out.println("エラーが発生しました。");
+            e.printStackTrace();
+        }
     }
 }
---
+++ BlackJack/src/UnsupportedSannerException.java
@@ -0,0 +1,7 @@
+//標準入力のオリジナル例外クラス
+public class UnsupportedSannerException extends RuntimeException {
+
+    public UnsupportedSannerException(String msg) {
+        super(msg);
+    }
+}
